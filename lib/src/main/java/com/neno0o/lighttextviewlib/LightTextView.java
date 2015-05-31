cab9049704c2860b88f42193645436a1f74f36ed
Neno0o <mike_memo2007@yahoo.com>
2015-05-31 15:09:30 +0300

    InitLightTextView

--- lib/src/main/java/com/neno0o/lighttextviewlib/LightTextView.java
+++ lib/src/main/java/com/neno0o/lighttextviewlib/LightTextView.java
@@ -1,10 +1,13 @@
 package com.neno0o.lighttextviewlib;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Matrix;
-import android.view.View;
+import android.util.TypedValue;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class LightTextView extends TextView {
@@ -17,6 +20,28 @@ public class LightTextView extends TextView {
 
     public LightTextView(Context context) {
         super(context, null);
+        initLightTextView();
+    }
+
+    private void initLightTextView() {
+
+        animation.setFillBefore(true);
+        animation.setFillAfter(true);
+        animation.setFillEnabled(true);
+
+        if (!(getLayoutParams() instanceof ViewGroup.LayoutParams)) {
+            FrameLayout.LayoutParams layoutParams =
+                    new FrameLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT);
+            setLayoutParams(layoutParams);
+        }
+
+        // default textview attributes
+        setGravity(android.view.Gravity.CENTER);
+        setTextColor(Color.WHITE);
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, 13);
+        setBackgroundColor(Color.RED);
     }
 
     private Animation animation = new Animation() {
@@ -29,10 +54,6 @@ public class LightTextView extends TextView {
     };
 
     public enum Position {
-        LEFT_TOP, RIGHT_TOP
-    }
-
-    public void setTargetView(View targetView, Position position) {
-
+        LEFT_CORNER, RIGHT_CORNER
     }
 }
